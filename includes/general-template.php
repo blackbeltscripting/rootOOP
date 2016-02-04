ba51253ffa71b1753ffbcf0e93c639d26d3c36e3
Eric Ghiraldello <ericg@arty-web-design.com>
2016-02-04 00:28:52 -0800

    Properly links CSS/JS

--- includes/general-template.php
+++ includes/general-template.php
@@ -6,9 +6,9 @@ function rO_header()
 		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 		<!-- Adds Font Awesome Libs -->
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-		<link href="'.SITE_URL.'includes/css/login.css" rel="stylesheet">
+		<link href="'.SITE_URL.'/rootOOP/includes/css/login.css" rel="stylesheet">
 		<!-- Add rootOOP Ajax & Login JS -->
-		<script type="text/javascript" src="'.SITE_URL.'includes/js/header.js"></script>';
+		<script type="text/javascript" src="'.SITE_URL.'rootOOP/includes/js/header.js"></script>';
 }
 
 function post_header($template_name = null)
